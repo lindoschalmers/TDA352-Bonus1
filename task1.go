696a162f882b7bd9bbc0236b3f5c1f0e970aa254
lindoschalmers <lindos@chalmers.se>
2025-12-27 15:44:38 +0100

    Task1: Changed findNumber func to findElement func

--- task1.go
+++ task1.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 )
 
-func firstNumber(hash string) (int, bool) {
-	for _, char := range hash {
-		if char >= '0' && char <= '9' {
-			num, _ := strconv.Atoi(string(char))
-			return num, true
-		}
+func firstElement(hash string) (int, bool) {
+	if len(hash) == 0 {
+		return 0, false
+	}
+
+	char := rune(hash[0]) // Get first character
+
+	// Check if it's a valid hex character
+	if char >= '0' && char <= '9' {
+		return int(char - '0'), true // 0-9
+	} else if char >= 'a' && char <= 'f' {
+		return int(char - 'a' + 10), true // 10-15
 	}
 	return 0, false
 }
@@ -19,12 +24,13 @@ func firstNumber(hash string) (int, bool) {
 func MerkleTree(FirstName string, SurName string) (int, int) {
 	HashInput := fmt.Sprintf("%s%s-MerkleTree", FirstName, SurName)
 	Hash := Hash(HashInput)
-	Number, err := firstNumber(Hash)
+	Number, err := firstElement(Hash)
 	if !err {
 		fmt.Println("No numbers found in hash for: ", HashInput)
 		fmt.Println("Hash: ", Hash)
 		os.Exit(1)
 	}
+	fmt.Println("Hash: ", Hash)
 	return Number, Number + 1
 
 }
